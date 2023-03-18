3d1910f7b0f9d392d50d45909c0d3bbd5d1c934d
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-18 10:47:02 +0530

    Logical Program Practice

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Fibonacci_Series_Test_Problem;
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        TestProgram.Fibonacciseriesproblem();
+        PerfectNumberProblem.FindPerfectNumber();
+        ReverseTheNumber.ReversetheNumbers();
+    }
+}
