fc3fc6efec85d555e3dff68f184b4d96abfd686e
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-18 10:50:12 +0530

    PerfectNumberProblem

---
+++ PerfectNumberProblem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class PerfectNumberProblem
+    {
+        public static void FindPerfectNumber()
+        {
+            int num, min, max, i, sum;
+
+            Console.WriteLine("Find the Pefect Numbers Within a Range");
+            Console.WriteLine("Please enter the Starting Range : ");
+            min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Ending Range : ");
+            max = int.Parse(Console.ReadLine());
+
+            for(num = min; num <= max; num++)
+            {
+                i = 1;
+                sum = 0;
+                while(i<num)
+                {
+                    if (num % i == 0)
+                        sum = sum + i;
+                    i++;
+
+                }
+                if (sum == num)
+                {
+                    Console.WriteLine(num + " ");
+                }
+            }
+        }
+    }
+}
