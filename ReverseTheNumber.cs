0d8dcb3281f3363fca92bf14ac9f934f6a79b36d
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-18 10:52:14 +0530

    Reverse the number problem

---
+++ ReverseTheNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class ReverseTheNumber
+    {
+        public static void ReversetheNumbers()
+        {
+            int num, reverse = 0, rem;
+            Console.WriteLine("Please Enter the Number whch you want to reverse : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num != 0)
+            {
+                rem = num % 10;
+                reverse = reverse * 10 + rem;
+                num = num / 10;
+                
+            }
+            Console.WriteLine("Reversed Number : " + reverse + " ");
+        }
+    }
+}
